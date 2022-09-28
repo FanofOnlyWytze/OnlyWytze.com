961ee67ff819a4933ccb178b8bf4f28f0aa8fa0f
FanofOnlyWytze <106373153+FanofOnlyWytze@users.noreply.github.com>
2022-09-28 16:59:34 +0200

    Add files via upload

--- quotereader.js
+++ quotereader.js
@@ -1,10 +1,11 @@
-var obj = fetch("../quotes.JSON")
-.then(res => res.text())
-.then(data => JSON.parse(data))
-
-console.log(obj)
+async function fetchQuotes() {
+    var obj = await fetch("https://www.onlywytze.com/quotes.json")
+    .then(res => res.text())
+    .then(data => JSON.parse(data))
+}
 
 function newQuote() {
+    var obj = fetchQuotes();
     var randomNumber = Math.floor(Math.random() * (obj.length));
     document.getElementById('quoteDisplay').innerHTML = obj;
 }
